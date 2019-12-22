0de7bce6961ec4291490d931fed1385f66859504
kvoyant <kvoyant@gmail.com>
2019-12-22 15:52:39 +0900

    어댑터 기초 작업 수행

---
+++ app/src/main/java/com/yhkim/loginandsignupapi_kt20191222/adapters/BlackListAdapter.kt
@@ -0,0 +1,33 @@
+package com.yhkim.loginandsignupapi_kt20191222.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.yhkim.loginandsignupapi_kt20191222.R
+import com.yhkim.loginandsignupapi_kt20191222.datas.BlackListData
+
+class BlackListAdapter(context:Context, resId:Int, list:ArrayList<BlackListData>) : ArrayAdapter<BlackListData>(context, resId, list) {
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        tempRow?.let {
+
+        }.let {
+            //null 일 경우
+            tempRow = inf.inflate(R.layout.black_list_item, null)
+        }
+
+        val row = tempRow!! // !! 절대 널이 아님
+
+
+
+
+        return row
+    }
+}
