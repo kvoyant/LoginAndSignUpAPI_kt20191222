557938be2a4ea88524001de7477cbe18a047eeb3
kvoyant <kvoyant@gmail.com>
2019-12-22 11:20:12 +0900

    회원가입 화면 초안 작성

---
+++ app/src/main/java/com/yhkim/loginandsignupapi_kt20191222/SignUpActivity.kt
@@ -0,0 +1,20 @@
+package com.yhkim.loginandsignupapi_kt20191222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+}
