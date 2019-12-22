4341adf6454a9c726253d10d7dae3a956334eb38
kvoyant <kvoyant@gmail.com>
2019-12-22 12:42:16 +0900

    성공에 따른 토스트 처리 - 에러 발생(앱 다이)

--- app/src/main/java/com/yhkim/loginandsignupapi_kt20191222/SignUpActivity.kt
+++ app/src/main/java/com/yhkim/loginandsignupapi_kt20191222/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.yhkim.loginandsignupapi_kt20191222
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.yhkim.loginandsignupapi_kt20191222.utils.ConnectServer
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import org.json.JSONObject
@@ -30,6 +31,19 @@ class SignUpActivity : BaseActivity() {
             ConnectServer.putRequestSignUp(mContext, inputId, inputPw, inputName, inputPhone, object: ConnectServer.JsonResponseHandler{
                 override fun onResponse(json: JSONObject) {
                     Log.d("회원가입응답", json.toString())
+
+//                    서버가 내려주는 code값이 몇인지 확인
+                    val code = json.getInt("code")
+                    Log.d("회원가입코드","${code}") //200
+
+                    if(code == 200 ) {
+                        Toast.makeText(mContext, "회원가입 성공 입니다.", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                    else {
+                        Toast.makeText(mContext, "회원가입 실패 입니다.", Toast.LENGTH_SHORT).show()
+                    }
+
                 }
 
             })
