202c6e6a5cd564a7dc08b2f42ae9828590656db0
kvoyant <kvoyant@gmail.com>
2019-12-22 12:20:08 +0900

    권한 설정 후 성공 - JSON 확인

--- app/src/main/java/com/yhkim/loginandsignupapi_kt20191222/SignUpActivity.kt
+++ app/src/main/java/com/yhkim/loginandsignupapi_kt20191222/SignUpActivity.kt
@@ -21,6 +21,12 @@ class SignUpActivity : BaseActivity() {
             val inputName = nameEdt.text.toString()
             val inputPhone = phoneEdt.text.toString()
 
+/*
+
+회원가입응답: {"code":200,"message":"회원가입 성공입니다.","data":{"user":{"id":32,"login_id":"yhkim","name":"chris","phone":"01012345678","memo":"새 회원","category":{"id":1,"title":"일반매장","color":"#FF0000"},"is_admin":false,"start_date":null,"expire_date":null,"created_at":"2019-12-22 03:18:43"},"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZCI6MzIsImxvZ2luX2lkIjoieWhraW0iLCJwYXNzd29yZCI6IjE4YjUzZjNiYThhMjIwNjRmMTg4OGRjNjJlOTZlYWJjIn0.HU0IDGIHPF2SgD1Xwg_JEy1IKX4i9J5Tyr54U34h2WV0yINVi5JJpOmvl6DR8IZRDLVV9uNwzWW00TUPhuwVKA"}}
+
+*/
+
             ConnectServer.putRequestSignUp(mContext, inputId, inputPw, inputName, inputPhone, object: ConnectServer.JsonResponseHandler{
                 override fun onResponse(json: JSONObject) {
                     Log.d("회원가입응답", json.toString())
